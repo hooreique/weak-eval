def9c43b56cd0a7b57500e31ad10d926af969658
Je-yeong Song <46372718+hooreique@users.noreply.github.com>
2023-04-23 19:38:54 +0900

    refactor: Remove channel assertion

--- src/domain/channel.js
+++ src/domain/channel.js
@@ -1,13 +1,8 @@
 
-const COMPLETE = { message: '채점 완료' };
-const TIMEOUT = { message: '채점이 너무 오래 걸립니다. 프로그램을 종료합니다.' };
+const COMPLETE = { name: 'Evaluation Complete' };
+const TIMEOUT = { name: 'Program Timeout' };
 
 export const channel = {
     COMPLETE,
     TIMEOUT,
-};
-
-export const channels = {
-    [COMPLETE]: COMPLETE,
-    [TIMEOUT]: TIMEOUT,
 };
