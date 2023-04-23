b45b891fce831a630348ebe66862b44259cadf20
Je-yeong Song <46372718+hooreique@users.noreply.github.com>
2023-04-23 17:29:09 +0900

    refactor: Remove dummy result in print

--- src/domain/result.js
+++ src/domain/result.js
@@ -1,19 +1,18 @@
 
+const PENDING = { message: '채점 중...' };
 const CORRECT = { message: '정답입니다.' };
 const INCORRECT = { message: '틀렸습니다.' };
 const TIMEOVER = { message: '시간 초과' };
 const TIMEOUT = { message: '채점 종료됨' };
+const UNKNOWN = { message: '알 수 없음' };
+
+export const getMessage = result => result?.message || UNKNOWN.message;
 
 export const result = {
+    PENDING,
     CORRECT,
     INCORRECT,
     TIMEOVER,
     TIMEOUT,
-};
-
-export const results = {
-    [CORRECT]: CORRECT,
-    [INCORRECT]: INCORRECT,
-    [TIMEOVER]: TIMEOVER,
-    [TIMEOUT]: TIMEOUT,
+    UNKNOWN,
 };
